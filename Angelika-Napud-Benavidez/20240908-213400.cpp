55fbc18de9040c00a12b3cb769753f2ecdb50cde
Angelika222 <angelikabenavidez21@gmail.com>
2024-09-09 07:41:20 +0800

    Update

---
+++ Angelika-Napud-Benavidez/20240908-213400.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string> // Include the string header file
+using namespace std;
+
+int main() {
+    // 1. Declare a Variable Name
+    string name;
+    
+    // 2. Assigning a Value to a Variable Name
+    name = "Angelika Napud Benavidez";
+    
+    // 3. Display/Output the Value of Variable Name
+    cout << "The name is: " << name << endl;
+    
+    return 0;
+}
